4e83629bac22502c428b45960b0c48aeda08ccb8
Willlasbury <asburyw229@gmail.com>
2023-04-08 12:26:23 -0700

    queryed data from openweather api

--- js/script.js
+++ js/script.js
@@ -2,27 +2,50 @@
 // TODO:  create a link to https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 let key = '433504929c3f849b429bb998b8932527'
 
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}').then( function(event){
-//     console.log("event.target:", event.target)
-// })
+function getWeather(lat, lon, key, unit){
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${unit}`).then( function(response){
+        return response.json()
+    }).then( function (data){
+        // TODO: return: the current weather conditions and future weather conditions
+        // TODO:   display city name, the date, an icon representation of weather
+                // conditions, the temperature, the humidity, and the wind speed
+        let conditions = data.list[0].main
+        console.log("conditions:", conditions)
+        let cityName = data.city.name
+        // console.log("cityName:", cityName)
+        // let futureWeather = something
+        // let date = dayjs.format(DDDD, MM YYYY)
+        let icon = data.list[0].weather[0].icon
+        console.log("icon:", icon)
+        let temp = conditions.temp
+        let humidity = conditions.humidity
+        let windSpeed = data.list[0].wind.speed
+        console.log("windSpeed:", windSpeed)
+        console.log("data:", data)
+    })
+}
 
 
 // TODO: set up geocoder api to convert lat, lon to cities
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-function getLatLon(city, key){
-
+async function getLatLon(city, key){
+    
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`).then(function(response){
-        console.log("response:", response)
         return response.json()
     }).then(function (data){
-        console.log("data:", data)
+        // console.log("data:", data)
         let latitude = data[0].lat
-        let longtitude = data[0].lon
-        return latitude, longtitude
+        let longitude = data[0].lon
+        // console.log("latitude, longitute:", latitude, longitude)
+        // TODO: send lat, lon to openweather
+        getWeather(latitude, longitude, key, 'imperial')
+        
     })
 }
 
-getLatLon('Seattle', key)
+let lat, lon = getLatLon('Seattle', key)
+// console.log("lat, longitude:", lat)
 
 // TODO: build function that pulls in the user input 
 function getCityIn() {
